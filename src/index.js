cb4b206be9753e20caf77df662a44ce1cc094212
Fabio Focking <focking@edu.univali.br>
2020-06-09 23:43:41 -0300

    root import

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {View} from 'react-native';
+import { View } from 'react-native';
 
 // import { Container } from './styles';
 
 export default function App() {
-  return <View />;
+    return <View />;
 }
