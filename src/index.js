92e332969f884efdc476b2ff94cc46216995ea41
Fabio Focking <focking@edu.univali.br>
2020-06-11 22:27:47 -0300

    ajuste nas versão do react-navigator

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { store, persistor } from './store';
 import { StatusBar } from 'react-native';
-import Routes from './routes';
+import App from '~/App';
 
 // import { Container } from './styles';
 
-export default function App() {
+export default function Index() {
     return (
         <Provider store={store}>
             <PersistGate persistor={persistor}>
                 <StatusBar barStyle="light-content" backgroundColor="black" />
-                <Routes />
+                <App />
             </PersistGate>
         </Provider>
     );
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,25 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+//import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+//import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import SignIn from '~/pages/SignIn';
 import SignUp from '~/pages/SignUp';
+import Dashboard from '~/pages/Dashboard';
 
-export default createAppContainer(
-    createSwitchNavigator({
-        SignIn,
-        SignUp,
-    })
-);
+export default (isSigned = false) =>
+    createAppContainer(
+        createSwitchNavigator(
+            {
+                Sign: createSwitchNavigator({
+                    SignIn,
+                    SignUp,
+                }),
+                App: createBottomTabNavigator({
+                    Dashboard,
+                }),
+            },
+            {
+                initialRouteName: isSigned ? 'App' : 'Sign',
+            }
+        )
+    );
