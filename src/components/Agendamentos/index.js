f1e2d8a4a34b78df0dc9316266a1ab213a134edb
Fabio Focking <focking@edu.univali.br>
2020-06-12 17:16:18 -0300

    pega agendamento da api e permite cancelar.

--- src/components/Agendamentos/index.js
+++ src/components/Agendamentos/index.js
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { formatRelative, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt-BR';
 import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { Container, Left, Avatar, Info, Name, Time } from './styles';
 
-export default function Agendamento() {
+export default function Agendamento({ data, onCancel }) {
+    const dateParsed = useMemo(() => {
+        return formatRelative(parseISO(data.date), new Date(), {
+            locale: pt,
+            addSuffix: true,
+        });
+    }, [data.date]);
     return (
-        <Container>
+        <Container past={data.past}>
             <Left>
                 <Avatar
                     source={{
-                        uri: 'https://api.adorable.io/avatar/50/sport.png',
+                        uri: data.recurso.avatar
+                            ? data.recurso.avatar.url
+                            : `https://api.adorable.io/avatar/50/${data.recurso.name}.png`,
                     }}
                 />
                 <Info>
-                    <Name>Fabio Focking</Name>
-                    <Time>há 3 horas</Time>
+                    <Name>{data.recurso.name}</Name>
+                    <Time>{dateParsed}</Time>
                 </Info>
             </Left>
-            <TouchableOpacity onPress={() => {}}>
-                <Icon name="event-busy" size={20} color="red" />
-            </TouchableOpacity>
+            {data.cancelable && !data.canceledAt && (
+                <TouchableOpacity onPress={onCancel}>
+                    <Icon name="event-busy" size={20} color="red" />
+                </TouchableOpacity>
+            )}
         </Container>
     );
 }
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-//import { View } from 'react-native';
 import Background from '../../components/Background';
 import { Container, Title, List } from './styles';
 import Agendamento from '~/components/Agendamentos';
-
-const data = [1, 2, 3, 4, 5];
+import api from '~/services/api';
 
 export default function Dashboard() {
+    const [agendamentos, setAgendamentos] = useState([]);
+    useEffect(() => {
+        async function loadAgendamentos() {
+            const response = await api.get('agendamentos');
+            setAgendamentos(response.data);
+        }
+        loadAgendamentos();
+    }, []);
+
+    async function handleCancel(id) {
+        const response = await api.delete(`agendamentos/${id}`);
+        setAgendamentos(
+            agendamentos.map(agendamento =>
+                agendamento.id === id
+                    ? {
+                          ...agendamento,
+                          canceledAt: response.data.canceledAt,
+                      }
+                    : agendamento
+            )
+        );
+    }
+
     return (
         <Background>
             <Container>
                 <Title>Agendamentos</Title>
                 <List
-                    data={data}
-                    keyExtrator={item => String(item)}
-                    renderItem={({ item }) => <Agendamento data={item} />}
+                    data={agendamentos}
+                    keyExtrator={item => String(item.id)}
+                    renderItem={({ item }) => <Agendamento onCancel={() => handleCancel(item.id)} data={item} />}
                 />
             </Container>
         </Background>
