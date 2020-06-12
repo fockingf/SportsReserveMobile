07de7ad6c352654a73ba45077d49ede9ed171c6b
Fabio Focking <focking@edu.univali.br>
2020-06-11 23:45:06 -0300

    agendamentos estilos

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,10 +2,24 @@ import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 //import { View } from 'react-native';
 import Background from '../../components/Background';
-// import { Container } from './styles';
+import { Container, Title, List } from './styles';
+import Agendamento from '~/components/Agendamentos';
+
+const data = [1, 2, 3, 4, 5];
 
 export default function Dashboard() {
-    return <Background />;
+    return (
+        <Background>
+            <Container>
+                <Title>Agendamentos</Title>
+                <List
+                    data={data}
+                    keyExtrator={item => String(item)}
+                    renderItem={({ item }) => <Agendamento data={item} />}
+                />
+            </Container>
+        </Background>
+    );
 }
 
 Dashboard.navigationOptions = {
